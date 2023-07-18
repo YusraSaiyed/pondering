1df0010927947ad936cf46f51c2675bc551669d2
Yusra Saiyed <yusrasaiyed14@gmail.com>
2023-07-18 11:28:42 -0700

    Initial Commit

---
+++ start page/start_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  start_pageApp.swift
+//  start page
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct start_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
